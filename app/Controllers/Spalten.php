48034ed6f75af09159212466cce4140fa33dc19a
Tim Böhmer <s4tiboeh@uni-trier.de>
2025-11-14 14:06:39 +0100

    update

---
+++ app/Controllers/Spalten.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controllers;
+
+class Spalten extends BaseController
+{
+    public function index()
+    {
+        return view('spalten');
+    }
+}
---
+++ app/Views/startseite.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>WE Team 01</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="https://team01.wi1cm.uni-trier.de/public/assets/css/style.css"  >
+</head>
+<body>
+<header>
+    <nav class="navbar navbar-expand-lg">
+
+        <div class="container-fluid">
+            <div class="nav d-flex w-100 align-items-center">
+
+                <img src="https://team01.wi1cm.uni-trier.de/public/assets/images/navicon.svg" class="img-fluid" alt="Logo" width="200" height="50">
+                <ul class="navbar-nav ms-auto d-flex flex-row">
+
+                    <li class="nav-item"><a class="nav-link" href="#">Tasks</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Boards</a></li>
+                    <li class="nav-item"><a class="nav-link" href="<?= base_url('spalten') ?>">Spalten</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
+
+<main>
+    <div class="card">
+
+        <div class="card-header">Tasks</div>
+
+        <div class="card-body">
+            <p class="card-text">hier entstehen neue Tasks...</p>
+        </div>
+        </div>
+    </div>
+</main>
+
+<footer class="footer fixed-bottom mt-auto">
+    <nav class="navbar navbar-expand-lg">
+
+        <div class="container-fluid">
+            <li class="nav-text">©Web-Entwicklung Team 01</li>
+            <div class="footer d-flex w-100 align-items-center">
+                <ul class="navbar-nav ms-auto d-flex flex-row">
+                    <li class="nav-item"><a class="nav-link" href="#">Impressum</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Datenschutz</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Kontakt</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</footer>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+</html>
+
+
