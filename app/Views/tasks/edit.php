f517fe928fa6b06871a99c572485d6f1c4414a25
Tim Böhmer <s4tiboeh@uni-trier.de>
2026-01-14 12:11:47 +0100

    update

--- app/Views/tasks/edit.php
+++ app/Views/tasks/edit.php
@@ -24,9 +24,9 @@
                     <div class="col-sm-10">
                         <select name="taskartenid" class="form-select" required>
                             <option value="">Taskart auswählen</option>
-                            <option value="1" <?= (isset($task['taskartenid']) && $task['taskartenid'] == 1) ? 'selected' : '' ?>>Aufgabe</option>
-                            <option value="2" <?= (isset($task['taskartenid']) && $task['taskartenid'] == 2) ? 'selected' : '' ?>>Bug</option>
-                            <option value="3" <?= (isset($task['taskartenid']) && $task['taskartenid'] == 3) ? 'selected' : '' ?>>Idee</option>
+                            <option value="1" <?= (isset($tasks['taskartenid']) && $tasks['taskartenid'] == 1) ? 'selected' : '' ?>>Aufgabe</option>
+                            <option value="2" <?= (isset($tasks['taskartenid']) && $tasks['taskartenid'] == 2) ? 'selected' : '' ?>>Bug</option>
+                            <option value="3" <?= (isset($tasks['taskartenid']) && $tasks['taskartenid'] == 3) ? 'selected' : '' ?>>Idee</option>
                         </select>
                     </div>
                 </div>
@@ -44,9 +44,9 @@
                     <label for="SpaltenID" class="col-sm-2 col-form-label">Status (SpaltenID):</label>
                     <div class="col-sm-10">
                         <select name="spaltenid" class="form-select" required>
-                            <option value="1" <?= (isset($task['spaltenid']) && $task['spaltenid'] == 1) ? 'selected' : '' ?>>ToDo</option>
-                            <option value="2" <?= (isset($task['spaltenid']) && $task['spaltenid'] == 2) ? 'selected' : '' ?>>In Bearbeitung</option>
-                            <option value="3" <?= (isset($task['spaltenid']) && $task['spaltenid'] == 3) ? 'selected' : '' ?>>Erledigt</option>
+                            <option value="1" <?= (isset($tasks['spaltenid']) && $tasks['spaltenid'] == 1) ? 'selected' : '' ?>>ToDo</option>
+                            <option value="2" <?= (isset($tasks['spaltenid']) && $tasks['spaltenid'] == 2) ? 'selected' : '' ?>>In Bearbeitung</option>
+                            <option value="3" <?= (isset($tasks['spaltenid']) && $tasks['spaltenid'] == 3) ? 'selected' : '' ?>>Erledigt</option>
                         </select>
                     </div>
                 </div>
@@ -65,8 +65,8 @@
                     <label for="Erinnerung" class="col-sm-2 col-form-label">Erinnerung:</label>
                     <div class="col-sm-10">
                         <select name="erinnerung" class="form-select">
-                            <option value="0" <?= (isset($task['erinnerung']) && $task['erinnerung'] == 0) ? 'selected' : '' ?>>Nein</option>
-                            <option value="1" <?= (isset($task['erinnerung']) && $task['erinnerung'] == 1) ? 'selected' : '' ?>>Ja</option>
+                            <option value="0" <?= (isset($tasks['erinnerung']) && $tasks['erinnerung'] == 0) ? 'selected' : '' ?>>Nein</option>
+                            <option value="1" <?= (isset($tasks['erinnerung']) && $tasks['erinnerung'] == 1) ? 'selected' : '' ?>>Ja</option>
                         </select>
                     </div>
                 </div>
